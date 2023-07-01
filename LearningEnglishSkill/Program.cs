9c07cfe6bdd283c8f4f7e292145970ea30c21661
Poy Chang <poypost@gmail.com>
2023-07-01 23:01:34 +0800

    first commit

---
+++ LearningEnglishSkill/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using System.Text;
+
+namespace LearningEnglishSkill
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // 初始化Kernel
+            var builder = new KernelBuilder();
+            builder.WithAzureChatCompletionService(
+                "text-davinci-003",
+                "{your azure openai endpoint}",
+                "{your azure openai key}");
+            var kernel = builder.Build();
+
+            // 導入技能
+            var skills = kernel.ImportSemanticSkillFromDirectory("Skills", "Learning");
+            var myContext = new ContextVariables();
+            var histories = new StringBuilder();
+
+            Console.WriteLine("Say anything to start practicing English.");
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                var input = Console.ReadLine();
+                // 填充變量
+                myContext.Set("history", histories.ToString());
+                myContext.Set("input", input);
+                //# 運行技能
+                var myResult = kernel.RunAsync(myContext, skills["LearningEnglishSkill"]).GetAwaiter().GetResult();
+                histories.AppendLine(input);
+                histories.AppendLine(myResult.Result.ToString());
+                Console.WriteLine(myResult);
+            }
+        }
+    }
+}
